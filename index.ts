75eb969c22ffdabf372a4df71e9cd11ca8828f7c
Axel Jäger <axeljaeger@gmail.com>
2024-02-03 22:38:36 +0100

    Include generated API, consume official GoPro API spec.

--- index.ts
+++ index.ts
@@ -1,76 +1,134 @@
 
 import express from 'express'
-import fetch from 'node-fetch';
 
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
 import { Command } from 'commander';
 
-const app = express()
-const port = 3006
+import { QueryApi } from './generate/apis/QueryApi.js';
+import { Configuration } from './generate';
 
-let cameraName = '';
-let cameraip: string | null = null;
+import open from 'open';
 
-async function getCameraName(goproip: string): Promise<string> {
-  // Fetch info from camera
+import { absolutePath } from 'swagger-ui-dist';
+
+async function getCameraName(cameraIp: string): Promise<string> {  
+  const configuration = new Configuration({basePath: `http://${cameraIp}`});
+  const api = new QueryApi(configuration);
   try {
-    const response = await fetch(`http://${goproip}/gopro/camera/state`);
-    const data = await response.json();
-    return data.status[30];
+    const response = await api.oGPGETSTATE();
+    const stateStatus = response.status;
+    return stateStatus?._30 ?? '';
   } catch (e) {
-    console.log("Error in fetch");
-    console.log(e);
+    console.log("Error in getting camera state:", e);
   }
   return '';
 }
 
-async function startCameraProxy(camIp : string) {
-    cameraName = await getCameraName(camIp);
-    const goprourl = `http://${cameraip}`;
+const startSwaggerServer = (app : express.Express) => {
+  const pathToSwaggerUi = absolutePath();
+  app.get('/swagger-initializer.js', (req, res, next) => {
+    res.send(`
+    window.onload = function() {  
+      window.ui = SwaggerUIBundle({
+        url: "https://raw.githubusercontent.com/gopro/OpenGoPro/main/docs/specs/.openapi.yml",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl,
+  
+          function() {
+            return {
+              statePlugins: {
+                spec: {
+                  wrapActions: {
+                    updateJsonSpec: function(oriAction, system) {
+                      return (spec) => {
+                            spec.servers = [{url: window.location.origin}]
+                            return oriAction(spec)
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        ],
+        layout: "StandaloneLayout"
+      });
+    };`);
+  });
+  app.use(express.static(pathToSwaggerUi))
+}
+
+const startCameraProxy = async (app : express.Express, cameraIp : string) => {
+    const goprourl = `http://${cameraIp}`;
     app.use('/gopro', createProxyMiddleware(
-      {
+    {
         target: goprourl,
         changeOrigin: true,
         selfHandleResponse: true,
         onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-          res.setHeader('Access-Control-Allow-Origin', 'https://axeljaeger.github.io');
+          res.setHeader('Access-Control-Allow-Origin', '*');
           return responseBuffer;
         }),
-      }));
-    app.listen(port, () => {
-      console.log(
-        `GoPro Development Server listening at http://localhost:${port}
-Camera IP: ${cameraip}
-Browse the API using http://axeljaeger.github.io/gopro-openapi`);
+    }));
+  };
+  
+
+const main = async () => {
+  const port = 3006
+  
+  const program = new Command();
+  program
+    .name("gopro-dev-server")
+    .version("1.0.0")
+    .description("Proxy for GoPro API")
+    .option("--ip <value>", "IP of camera to connect to")
+    .option("--wifi", "Use the static IP of the camera's wifi network, i.e. 10.5.5.9")
+    .option("--usb-serial <value>", "Use the last three digits of the camera's serial number to determine the camera's IP for USB connection.")
+    .parse(process.argv);
+
+  const opts = program.opts();
+
+  let cameraip: string | null = null;
+
+  if (opts.ip) {
+    cameraip = opts.ip;
+  } else if (opts.wifi) {
+    cameraip = '10.5.5.9';
+  } else if (opts.usbSerial) {
+    const serialStub = opts.usbSerial;
+    cameraip = `172.2${serialStub[0]}.1${serialStub[1]}${serialStub[2]}.51`;
+  }
+
+  if (cameraip !== null) {
+    getCameraName(cameraip).then((cameraName) => {
+      if (cameraName) {
+        const app = express();
+        startCameraProxy(app, cameraip!);
+        startSwaggerServer(app);
+
+        app.listen(port, () => {
+          console.log(
+            `GoPro Development Server listening at http://localhost:${port}
+Camera Name: ${cameraName}
+Camera IP: ${cameraip}`);
+          open(`http://localhost:${port}`);
+        });
+      }
+      else {
+        console.log(`Could not connect to camera at ${cameraip}.`);
+      }
     });
+  } else {
+    program.help();
+  }
 }
 
-const program = new Command();
+export default main;
 
-program
-  .name("gopro-dev-server")
-  .version("1.0.0")
-  .description("Proxy for GoPro API")
-  .option("--ip <value>", "IP of camera to connect to")
-  .option("--wifi", "Use the static IP of the camera's wifi network, i.e. 10.5.5.9")
-  .option("--usb-serial <value>", "Use the last three digits of the camera's serial number to determine the camera's IP for USB connection.")
-  .addHelpText('after', `If none of the above specified, gopro-dev-server will try to detect the camera in the local network.`)
-  .parse(process.argv);
-
-const opts = program.opts();
-
-if (opts.ip) {
-  console.log(opts.ip);
-  cameraip = opts.ip;
-} else if (opts.wifi) {
-  cameraip = '10.5.5.9';
-} else if (opts.usbSerial) {
-  const serialStub = opts.usbSerial;
-  cameraip = `172.2${serialStub[0]}.1${serialStub[1]}${serialStub[2]}.51`;
-}
-
-if (cameraip) {
-  startCameraProxy(cameraip);
-} else {
-  program.help();
-}
+await main();
