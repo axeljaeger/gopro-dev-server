4a93784a1161bd0b50d7900cc8bf7513d168afc9
Axel Jäger <axeljaeger@gmail.com>
2024-02-04 14:30:20 +0100

    Add generated API.

---
+++ generate/apis/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnalyticsApi.js';
+export * from './COHNApi.js';
+export * from './ControlApi.js';
+export * from './HilightsApi.js';
+export * from './MediaApi.js';
+export * from './OTAApi.js';
+export * from './PresetsApi.js';
+export * from './PreviewStreamApi.js';
+export * from './QueryApi.js';
+export * from './SettingsApi.js';
+export * from './WebcamApi.js';
